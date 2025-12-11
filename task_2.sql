d0bad8a9113ebc6b9b86a2cd7200f6e0c4bd6d6d
Shema Manasseh <shemamanase992@gmail.com>
2025-12-11 12:32:27 +0300

    ...

--- task_2.sql
+++ task_2.sql
@@ -39,7 +39,7 @@ CREATE TABLE Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity FLOAT,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
